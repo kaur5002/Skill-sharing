c65b9adf1e9012ba0960847cd501e7a0af267e15
Satvir Kaur <satvir2824018@gmail.com>
2025-09-13 13:23:23 -0500

    project workflow

---
+++ online-learning/src/lib/mongodb.ts
@@ -0,0 +1,27 @@
+// lib/mongodb.ts
+import { MongoClient } from "mongodb";
+
+if (!process.env.MONGODB_URI) {
+  throw new Error("Please define the MONGODB_URI environment variable");
+}
+
+const uri = process.env.MONGODB_URI;
+const options = {};
+
+let client;
+let clientPromise: Promise<MongoClient>;
+
+if (process.env.NODE_ENV === "development") {
+  // In development, use a global variable so the client is reused
+  if (!(global as any)._mongoClientPromise) {
+    client = new MongoClient(uri, options);
+    (global as any)._mongoClientPromise = client.connect();
+  }
+  clientPromise = (global as any)._mongoClientPromise;
+} else {
+  // In production, create a new client
+  client = new MongoClient(uri, options);
+  clientPromise = client.connect();
+}
+
+export default clientPromise;
